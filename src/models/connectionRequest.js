8a1e3dbe039a59acd491c381ab6dc6b53d3f8ada
riyamotani <riyamotani601@gmail.com>
2025-06-14 20:33:06 +0530

    minor fix

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -8,6 +8,7 @@ const connectionRequestSchema = mongoose.Schema({
     },
     toUserId:{
         type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true,
     },
     status:{
